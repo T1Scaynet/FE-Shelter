9c967ab9133151ea85bfc257a669b1db4c3fc422
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-03-13 18:09:01 -0300

    fix: bugs routes

---
+++ src/views/Home/index.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div className='bg-black w-full h-screen flex items-center justify-center'>
+      <p className='text-red-500 text-5xl'>Home</p>
+    </div>
+  );
+};
