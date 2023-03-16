efda6725307b702eb05c1ebfc76bc1d4a5e0de7a
ivan <leon37728@gmail.com>
2023-03-16 11:52:57 -0400

    feat: display three section added

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,13 +1,15 @@
-
 import '../../App.css';
 import { DisplayOne } from './DisplayOne';
 import NavBar from '../../components/NavBar';
-      
+import { DisplayThree } from './DisplayThree';
+
 export const Home = () => {
   return (
     <div>
       <NavBar />
       <DisplayOne />
+
+      <DisplayThree />
     </div>
   );
 };
