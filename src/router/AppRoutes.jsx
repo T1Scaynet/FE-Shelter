c644300d1d4e6614cc837fc93512e3bff2ab0aca
RomiMolina <roomi.molina@gmail.com>
2023-03-17 21:28:52 -0300

    feat page error

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../views/Home';
 import { PetsList } from '../views/PetsList';
+import { Error404 } from '../views/Error404';
 
 export function AppRoutes () {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/completeList' element={<PetsList />} />
+      <Route path='/error' element={<Error404 />} />
     </Routes>
   );
 };
--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import pets from './features/pets/petSlice';
 import petDetails from './features/details/detailSlice';
+import errorSlice from './features/error/errorSlice';
 
 export const store = configureStore({
   reducer: { // unifica los reducer que cada estado en un solo lugar
     pets,
-    petDetails
+    petDetails,
+    error: errorSlice
   }
 });
