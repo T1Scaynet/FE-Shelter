a030e303764574ec2ad86ef24f470ada12988c74
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-03-20 12:20:01 -0300

    feat: top state added

--- src/state/store.js
+++ src/state/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import pets from './features/pets/petSlice';
 import petDetails from './features/details/detailSlice';
 import errorSlice from './features/error/errorSlice';
+import top from './features/top/topSlice';
 
 export const store = configureStore({
   reducer: { // unifica los reducer que cada estado en un solo lugar
     pets,
     petDetails,
-    error: errorSlice
+    error: errorSlice,
+    top
   }
 });
