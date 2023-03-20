a1243e18cb7fc6c62bb2486b4d341d996aa9d5a5
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-03-20 12:08:53 -0300

    fix: component added in the sprit operation

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,30 +1,32 @@
 import { Link } from 'react-router-dom';
 import { Card } from '../Card';
+import { PetsNotFound } from '../PetsNotFound';
 
 export const Cards = ({ pets }) => {
   const allPets = pets.list;
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-2 gap-3 gap-x-4 2xl:gap-x-[0.5rem] rounded-md'>
-      {allPets.length > 0
-        ? (
-            allPets.map((p) => (
-              <Link key={p.id} to={`/detalles/${p._id}`}>
-                <Card
-                  key={p._id}
-                  name={p.name}
-                  genres={p.genre}
-                  size={p.size}
-                  age={p.age}
-                  image={p.image}
-                  history={p.history}
-                  db='true'
-                />
-              </Link>
-            ))
-          )
-        : (
-          <h1> No se encontró mascotas </h1>
-          )}
+    <div>
+      {
+        allPets.length
+          ? (
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-3 gap-x-4 2xl:gap-x-[0.5rem] rounded-md'>
+              {allPets.map((p) => (
+                <Link key={p.id} to={`/detalles/${p._id}`}>
+                  <Card
+                    key={p._id}
+                    name={p.name}
+                    genres={p.genre}
+                    size={p.size}
+                    age={p.age}
+                    image={p.image}
+                    history={p.history}
+                    db='true'
+                  />
+                </Link>
+              ))}
+            </div>)
+          : <PetsNotFound />
+      }
     </div>
   );
 };
