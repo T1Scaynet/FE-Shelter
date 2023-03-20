ccfd86fac4ade6e11418210de60f5c0ba5583d0f
Estani02 <estaniolmedo@gmail.com>
2023-03-20 15:36:54 -0300

    fix: code fix

--- src/views/DetailScreen/index.jsx
+++ src/views/DetailScreen/index.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import { clearDetailList, getDetailById } from '../../state/features/details/detailSlice';
+import { getDetailById } from '../../state/features/details/detailSlice';
 import bg from '../../assets/DetailScreen/bg.svg';
 import weight from '../../assets/DetailScreen/peso.svg';
 import genre from '../../assets/DetailScreen/genero.svg';
@@ -24,67 +24,70 @@ export const DetailScreen = () => {
     window.scrollTo(0, 0);
     dispatch(getDetailById(undefined));
     dispatch(getDetailById(id));
-    dispatch(clearDetailList());
-  }, [dispatch]);
+  }, [dispatch, id]);
 
   console.log(pet.history);
 
-  const settings = {
-    dots: true,
-    lazyLoad: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    initialSlide: 2
-  };
+  // const settings = {
+  //   dots: true,
+  //   lazyLoad: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   initialSlide: 2
+  // };
 
-  const settingsVertical = {
-    dots: false,
-    infinite: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    vertical: true,
-    verticalSwiping: true,
-    swipeToSlide: true,
-    beforeChange: function (currentSlide, nextSlide) {
-      console.log('before change', currentSlide, nextSlide);
-    },
-    afterChange: function (currentSlide) {
-      console.log('after change', currentSlide);
-    }
-  };
+  // const settingsVertical = {
+  //   dots: false,
+  //   infinite: false,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   vertical: true,
+  //   verticalSwiping: true,
+  //   swipeToSlide: true,
+  //   beforeChange: function (currentSlide, nextSlide) {
+  //     console.log('before change', currentSlide, nextSlide);
+  //   },
+  //   afterChange: function (currentSlide) {
+  //     console.log('after change', currentSlide);
+  //   }
+  // };
 
   return (
     <div className='flex justify-center items-center flex-col'>
-      <span className='flex justify-start items-center space-x-2 bg-[#EBE5F7] h-[4.875rem] w-full'>
-        <Link
-          to='/'
-          className='font-bold ml-[12.813rem] text-[#7C58D3] text-[0.9rem]'
-        >
-          Inicio
-        </Link>
-        <div className=' rounded-[50%] bg-[#FF47A2] h-[0.4rem] w-[0.4rem]' />
-        <Link
-          to='/completeList'
-          className='font-bold ml-[12.813rem] text-[#7C58D3] text-[0.9rem]'
-        >
-          Adoptar
-        </Link>
-        <div className=' rounded-[50%] bg-[#FF47A2] h-[0.4rem] w-[0.4rem]' />
-        <Link
-          to='/detalles'
-          className='font-bold ml-[12.813rem] text-[#7C58D3] text-[0.9rem]'
-        >
-          Detalles
-        </Link>
-      </span>
       {pet === undefined ? (
-        <div>Cargando...</div>
+        <div class='conteneinerLoading'>
+          <div class='loader' id='loader'>
+            Loading...
+          </div>
+        </div>
       ) : pet === null ? (
         <></>
       ) : (
         <>
+          <span className='flex justify-start items-center space-x-2 bg-[#FBF9FF] h-[4.875rem] w-full'>
+            <Link
+              to='/'
+              className='font-bold ml-[12.813rem] text-[#7C58D3] text-[0.9rem]'
+            >
+              Inicio
+            </Link>
+            <div className=' rounded-[50%] bg-[#FF47A2] h-[0.4rem] w-[0.4rem]' />
+            <Link
+              to='/completeList'
+              className='font-bold ml-[12.813rem] text-[#7C58D3] text-[0.9rem]'
+            >
+              Adoptar
+            </Link>
+            <div className=' rounded-[50%] bg-[#FF47A2] h-[0.4rem] w-[0.4rem]' />
+            <Link
+              to='#'
+              className='font-bold ml-[12.813rem] text-[#7C58D3] text-[0.9rem]'
+            >
+              Detalles
+            </Link>
+          </span>
           <section className='flex flex-row items-center space-x-10 mt-[0.9rem]'>
             {/* <Slider {...settingsVertical} className='w-[10.188rem]  bg-[#FBF9FF] flex justify-between'>
               {pet.galery?.map((img, i) => (
@@ -96,7 +99,9 @@ export const DetailScreen = () => {
                 <img src={img} alt='pet' className='object-cover w-[22.938rem] h-[22.938rem] rounded-[0.5rem] m-auto' key={i} />
               ))}
             </Slider> */}
-            <img src={pet.image} alt={pet.image} className='h-96 rounded-md' />
+            <div className='bg-[#FBF9FF] w-[31.313rem] h-[36.063rem] flex items-center justify-center rounded-md'>
+              <img src={pet.image} alt={pet.image} className='h-96 rounded-md' />
+            </div>
             <div className='flex flex-col items-start w-[20.938rem]'>
               <h4 className='text-[4.125rem] font-bold'>{pet.name}</h4>
               <div className='text-[1.625rem] font-bold flex flex-col justify-center text-left'>
@@ -144,7 +149,7 @@ export const DetailScreen = () => {
             </div>
           </section>
           {pet.history === undefined ? (
-            <div className='bg-[#EBE5F7] rounded-[0.5rem] mt-[6.875rem] w-[68.875rem] h-[10rem] flex items-center justify-center'>
+            <div className='bg-[#FBF9FF] rounded-[0.5rem] mt-[6.875rem] w-[68.875rem] h-[10rem] flex items-center justify-center border-solid border-black border-2'>
               <div className='text-center'>
                 <h4 className='text-[1.5rem] font-bold'>
                   ¡Adopta un amigo peludo y cambia su vida para siempre!
@@ -152,7 +157,7 @@ export const DetailScreen = () => {
               </div>
             </div>
           ) : (
-            <div className='bg-[#EBE5F7] rounded-[0.5rem] mt-[6.875rem] w-[68.875rem] h-[10rem] flex items-center justify-center'>
+            <div className='bg-[#FBF9FF] rounded-[0.5rem] mt-[6.875rem] w-[68.875rem] h-[10rem] flex items-center justify-center border-solid border-black border-2'>
               <div className='text-center'>
                 <h4 className='text-[1.5rem] font-bold'>
                   Descripción de la mascota
