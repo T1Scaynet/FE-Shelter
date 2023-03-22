c886a1a546ad58b5a6352a076707c68928a06ba2
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-03-22 14:40:44 -0300

    feat: division between public and private routes

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,18 +1,48 @@
 import { Route, Routes } from 'react-router-dom';
-import { DetailScreen } from '../views/DetailScreen';
-import { CreatePetScreen } from '../views/CreatePetScreen';
+import { useSelector } from 'react-redux';
 import { Home } from '../views/Home';
 import { PetsList } from '../views/PetsList';
-import { Error404 } from '../views/Error404';
+import { DetailScreen } from '../views/DetailScreen';
+import Error404 from '../views/Error404';
+import { CreatePetScreen } from '../views/CreatePetScreen';
 
 export function AppRoutes () {
+  const user = useSelector(state => state.users);
+  // const isAuth = user.user.token;
+  const isAdmin = user?.isAdmin;
+  const routes = [
+    {
+      path: '/',
+      component: Home,
+      public: true
+    },
+    {
+      path: '/listado',
+      component: PetsList,
+      // public: !isAuth
+      public: true
+    },
+    {
+      path: '/detalles/:id',
+      component: DetailScreen,
+      public: true
+    },
+    {
+      path: '/creacion-de-mascota',
+      component: CreatePetScreen,
+      public: isAdmin
+    }
+  ];
+
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/listado' element={<PetsList />} />
-      <Route path='/creacion-de-mascota' element={<CreatePetScreen />} />
-      <Route path='/detalles/:id' element={<DetailScreen />} />
+      {
+        routes.map(r => (
+          r.public &&
+            <Route key={r.path} path={r.path} element={<r.component />} />
+        ))
+      }
       <Route path='*' element={<Error404 />} />
     </Routes>
   );
-}
+};
