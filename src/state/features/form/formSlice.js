281db034b44b02ff698e2688901825adc7def822
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-03-25 00:09:45 -0300

    chore: base url change

--- src/state/features/form/formSlice.js
+++ src/state/features/form/formSlice.js
@@ -18,7 +18,7 @@ export default formSlice.reducer;
 export const PostForm = (payload) => {
   return async function () {
     try {
-      const formAxios = await axios.post('http://localhost:3001/form', payload);
+      const formAxios = await axios.post('/form', payload);
       return formAxios;
     } catch (error) {
       console.warn("Error al enviar datos en función 'PostPet'");
