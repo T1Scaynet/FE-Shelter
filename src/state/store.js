b298185de3459d3e2c96e4ff122d7d21db32e033
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-03-24 23:46:45 -0300

    feat: redux persist setup

--- src/state/store.js
+++ src/state/store.js
@@ -1,16 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import pets from './features/pets/petSlice';
 import petDetails from './features/details/detailSlice';
 import errorSlice from './features/error/errorSlice';
 import top from './features/top/topSlice';
 import users from './features/users/userSlice';
 
-export const store = configureStore({
-  reducer: {
-    users,
-    pets,
-    petDetails,
-    error: errorSlice,
-    top
-  }
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const rootReducer = combineReducers({
+  users,
+  pets,
+  petDetails,
+  error: errorSlice,
+  top
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
+});
+export const persistor = persistStore(store);
