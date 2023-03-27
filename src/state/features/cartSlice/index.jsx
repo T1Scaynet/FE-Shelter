98f547d42f87433b7c674cb2f4d1ebb1d7b46d7f
Gustavo Chura <gustavochura94@gmail.com>
2023-03-27 13:33:21 -0300

    feat: dispatch donation created

--- src/state/features/cartSlice/index.jsx
+++ src/state/features/cartSlice/index.jsx
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 // Toast message Config
@@ -12,7 +14,7 @@ const Toast = Swal.mixin({
   didOpen: (toast) => {
     toast.addEventListener('mouseenter', Swal.stopTimer);
     toast.addEventListener('mouseleave', Swal.resumeTimer);
-  },
+  }
 });
 
 // Updating localStorage cartItems function
@@ -25,16 +27,16 @@ const initialState = {
     ? JSON.parse(localStorage.getItem('cartItems'))
     : [],
   cartTotalQuantity: 0,
-  cartTotalAmount: 0,
+  cartTotalAmount: 0
 };
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addToCart(state, action) {
+    addToCart (state, action) {
       const itemIndex = state.cartItems.findIndex(
-        (item) => item.id === action.payload.id,
+        (item) => item.id === action.payload.id
       );
 
       if (itemIndex >= 0) {
@@ -42,7 +44,7 @@ const cartSlice = createSlice({
 
         Toast.fire({
           icon: 'success',
-          title: `${action.payload.title} sumado al carrito`,
+          title: `${action.payload.title} sumado al carrito`
         });
       } else {
         const tempProduct = { ...action.payload, cartQuantity: 1 };
@@ -50,53 +52,53 @@ const cartSlice = createSlice({
         // console.log(action.payload);
         Toast.fire({
           icon: 'success',
-          title: `${action.payload.title} agregado al carrito`,
+          title: `${action.payload.title} agregado al carrito`
         });
       }
       updateLocalStorage(state);
     },
-    removeFromCart(state, action) {
+    removeFromCart (state, action) {
       const nextCartItems = state.cartItems.filter(
-        (cartItem) => cartItem.id !== action.payload.id,
+        (cartItem) => cartItem.id !== action.payload.id
       );
 
       state.cartItems = nextCartItems;
       updateLocalStorage(state);
       Toast.fire({
         icon: 'info',
-        title: `${action.payload.title} eliminado del carrito`,
+        title: `${action.payload.title} eliminado del carrito`
       });
     },
-    decreaseCart(state, action) {
+    decreaseCart (state, action) {
       const itemIndex = state.cartItems.findIndex(
-        (cartItem) => cartItem.id === action.payload.id,
+        (cartItem) => cartItem.id === action.payload.id
       );
 
       if (state.cartItems[itemIndex].cartQuantity > 1) {
         state.cartItems[itemIndex].cartQuantity -= 1;
       } else if (state.cartItems[itemIndex].cartQuantity === 1) {
         const nextCartItems = state.cartItems.filter(
-          (cartItem) => cartItem.id !== action.payload.id,
+          (cartItem) => cartItem.id !== action.payload.id
         );
 
         state.cartItems = nextCartItems;
         Toast.fire({
           icon: 'info',
-          title: ` ${action.payload.title} eliminado del carrito`,
+          title: ` ${action.payload.title} eliminado del carrito`
         });
       }
       updateLocalStorage(state);
     },
-    clearCart(state, action) {
+    clearCart (state, action) {
       state.cartItems = [];
       Toast.fire({
         icon: 'info',
-        title: 'Donaciones eliminadas del carrito',
+        title: 'Donaciones eliminadas del carrito'
       });
       updateLocalStorage(state);
     },
-    getTotals(state, action) {
-      let { total, quantity } = state.cartItems.reduce(
+    getTotals (state, action) {
+      const { total, quantity } = state.cartItems.reduce(
         (cartTotal, cartItem) => {
           const { price, cartQuantity } = cartItem;
           const itemTotal = price * cartQuantity;
@@ -108,16 +110,33 @@ const cartSlice = createSlice({
         },
         {
           total: 0,
-          quantity: 0,
-        },
+          quantity: 0
+        }
       );
       state.cartTotalAmount = total;
       state.cartTotalQuantity = quantity;
-    },
-  },
+    }
+  }
 });
 
 export const { addToCart, removeFromCart, decreaseCart, clearCart, getTotals } =
   cartSlice.actions;
+
+export const donation = (payload) => {
+  return async (_dispatch, getState) => {
+    const currentState = getState().login;
+    try {
+      const instance = axios.create();
+      instance.defaults.headers.common['x-access-token'] = currentState.token;
+      const donations = await instance.post('/payment', payload);
+      console.log(donations.data.body.init_point);
+      // navigate(donations.data.body.init_point);
+      window.location.href = donations.data.body.init_point;
+      return donations;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
 
 export default cartSlice.reducer;
