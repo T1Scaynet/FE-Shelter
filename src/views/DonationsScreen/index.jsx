97cf208a0018c273c7092a78160f1365376ee375
RomiMolina <roomi.molina@gmail.com>
2023-03-27 22:29:10 -0300

    fix buggs cart

--- src/views/DonationsScreen/index.jsx
+++ src/views/DonationsScreen/index.jsx
@@ -1,53 +1,11 @@
-/* eslint-disable multiline-ternary */
-/* eslint-disable react/self-closing-comp */
-// import { useGetAllProductsQuery } from '../../state/features/products/productsApi';
 import { DonationCard } from './DonationCard';
 import { donations } from '../../utils/products';
-import Swal from 'sweetalert2';
-import { useLocation } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { clearCart } from '../../state/features/cartSlice';
 
 export const DonationsScreen = () => {
-  const dispatch = useDispatch();
-
-  const { search } = useLocation();
-
-  const query = new URLSearchParams(search);
-  const status = query.get('collection_status');
-  if (status === 'rejected') {
-    Swal.fire({
-      icon: 'error',
-      title: 'La transacción ha sido rechazada',
-      timer: '2000',
-    });
-  } else if (status === 'approved') {
-    Swal.fire({
-      icon: 'success',
-      title: 'La transacción ha sido exitosa',
-      timer: '2000',
-    });
-    dispatch(clearCart());
-  }
-  // const { data, error, isLoading } = useGetAllProductsQuery();
-
   return (
-    <div className="px-8 w-screen h-screen">
-      <h2 className="text-center">Donaciones</h2>
-      <div className="flex justify-center items-center gap-6 ">
-        {/* {isLoading ? (
-          <p>Cargando información</p>
-        ) : error ? (
-          <>
-            <p>Ocurrió un error. No recibe información del servidor</p>
-            <p>El componente funciona envienme algo del back </p>
-          </>
-        ) : (
-          data?.map((product) => (
-            <DonationCard key={product.id} product={product} />
-          ))
-        )} */}
-
+    <div className='px-8 w-screen h-full'>
+      <h2 className='text-center text-4xl font-bold mb-4'>Donaciones</h2>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4'>
         {donations?.map((product) => (
           <DonationCard key={product.id} product={product} />
         ))}
