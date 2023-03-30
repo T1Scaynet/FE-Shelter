cfae2958593a3be7859ae532f86d1ed78a809b5a
Gustavo Chura <gustavochura94@gmail.com>
2023-03-30 18:29:39 -0300

    feat: pets dashboard added

---
+++ src/views/Admin - Dashboard/pages/Mascotas/Lista-de-Mascotas.jsx
@@ -0,0 +1,11 @@
+import Breadcrumb from "../../components/Breadcrumb"
+import DefaultLayout from "../../layout/DefaultLayout"
+
+export const ListadeMascotas = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName='Lista de Mascotas'/>
+      <div>Lista de Mascotas</div>
+    </DefaultLayout>
+  )
+}
