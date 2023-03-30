354a2d7983d6bd69287060f0a71132ee602c05f1
Gustavo Chura <gustavochura94@gmail.com>
2023-03-30 18:18:32 -0300

    feat: do not show component navbar or footer in component error404

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@ import { AppRoutes } from './router/AppRoutes';
 import { NavBar } from './components/NavBar';
 import { Footer } from './components/Footer';
 import { Toaster } from 'sonner';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function App () {
+  const [showLayout, setShowLayout] = useState(true);
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  useEffect(() => {
+    currentPath && currentPath.includes('admin')
+      ? setShowLayout(false)
+      : setShowLayout(true);
+  }, [currentPath]);
+
   return (
     <>
       <Toaster position='bottom-left' richColors closeButton />
-      <NavBar />
-      <AppRoutes />
-      <Footer />
+      {showLayout && <NavBar />}
+      <AppRoutes setShowLayout={setShowLayout} />
+      {showLayout && <Footer />}
     </>
   );
 }
