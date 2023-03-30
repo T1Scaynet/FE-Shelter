fcfada04a223ef509fc709e47c31cc2482d75624
Gustavo Chura <gustavochura94@gmail.com>
2023-03-30 18:29:24 -0300

    feat: donation dashboard added

---
+++ src/views/Admin - Dashboard/pages/Donaciones/Donaciones.jsx
@@ -0,0 +1,11 @@
+import Breadcrumb from '../../components/Breadcrumb';
+import DefaultLayout from '../../layout/DefaultLayout';
+
+export const DonacionesPagos = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName='Donaciones' />
+      <div>Donaciones</div>
+    </DefaultLayout>
+  );
+};
