ea9ae5793cdfec722f7f87d9c86952f144697d7b
MadridSelene <seleneailenmadrid@gmail.com>
2023-03-31 11:17:33 -0300

    responsive-error404

--- src/views/Error404/index.jsx
+++ src/views/Error404/index.jsx
@@ -23,25 +23,27 @@ export const Error404 = ({ setShowLayout }) => {
   }, []);
 
   return (
-    <div className='min-h-screen grid grid-rows-[300px]  place-content-center bg-[#FFDA47] py-12 px-4 sm:px-6 lg:px-8'>
+    <div className='min-h-screen grid grid-cols-1 md:grid-cols-1 place-content-center bg-[#FFDA47] py-12 px-4 sm:px-6 lg:px-8 space-y-5'>
       <div className='flex place-content-center'>
-        <h2 className=' text-[12.5rem] font-extrabold text-[#0E081E]'>4</h2>
-        <img src={img} alt={img} />
-        <h2 className='text-[12.5rem] font-extrabold text-[#0E081E]'>4</h2>
+        <h2 className='text-5xl md:text-7xl lg:text-9xl font-extrabold text-[#0E081E]'>4</h2>
+        <div className='w-13 md:w-30'>
+          <img src={img} alt={img} />
+        </div>
+        <h2 className='text-5xl md:text-7xl lg:text-9xl font-extrabold text-[#0E081E]'>4</h2>
       </div>
-      <div className=' w-full space-y-8 grid place-items-center'>
+      <div className='w-full space-y-8 grid place-items-center'>
         <div>
-          <h2 className='mt-6 text-center text-[4.25rem] font-extrabold text-gray-900'>
+          <h2 className='text-3xl md:text-4xl lg:text-5xl font-extrabold text-gray-900 text-center'>
             Oops! Ha ocurrido un error
           </h2>
-          <p className='mt-2 text-center text-lg text-[#0E081E]'>
+          <p className='mt-2 text-base md:text-lg lg:text-xl text-[#0E081E] text-center'>
             {errorMessage || 'Lo sentimos, algo salió mal.'}
           </p>
         </div>
         <div className='mt-8'>
           <button
-            className='w-[16.25rem] h-[4.3rem] mx-auto py-2 px-6 border border-transparent rounded-md shadow-sm text-sm font-medium
-            text-white bg-[#7C58D3] hover:bg-white hover:text-gray-700 hover:border-[#7C58D3] focus:outline-none hover:border-[0.09rem]'
+            className='w-full md:w-[16.25rem] h-[4.3rem] mx-auto py-2 px-6 border border-transparent rounded-md shadow-sm text-sm md:text-base lg:text-lg
+            text-white bg-[#7C58D3] hover:bg-white hover:text-[#7C58D3] hover:border-[#7C58D3] focus:outline-none hover:border-[0.09rem]'
             onClick={handleClick}
           >
             Volver al inicio
