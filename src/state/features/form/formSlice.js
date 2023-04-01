a1b028066ef70eb4598272b79e0a211abf58a4dd
Gustavo Chura <gustavochura94@gmail.com>
2023-04-01 16:36:57 -0300

    feat: removed custom headers so they can be global and avoid putting in the places that were needed

--- src/state/features/form/formSlice.js
+++ src/state/features/form/formSlice.js
@@ -16,12 +16,9 @@ export const formSlice = createSlice({
 export default formSlice.reducer;
 
 export const PostForm = (payload) => {
-  return async function (_dispatch, getState) {
-    const currentState = getState().login;
+  return async function () {
     try {
-      const instance = axios.create();
-      instance.defaults.headers.common['x-access-token'] = currentState.token;
-      const formAxios = await instance.post('/form', payload);
+      const formAxios = await axios.post('/form', payload);
       return formAxios;
     } catch (error) {
       console.warn("Error al enviar datos en función 'PostPet'");
