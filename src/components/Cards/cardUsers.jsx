f949336b3788cb82f2d4e33f321f336ec69665c3
rojassrodrigo <rojasrodrigo@live.com>
2023-04-01 22:19:32 -0300

    feat: dashboard user 2

--- src/components/Cards/cardUsers.jsx
+++ src/components/Cards/cardUsers.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 import { Card } from '../Card';
 import { PetsNotFound } from '../PetsNotFound';
 
-export const Cards = ({ users }) => {
-  const allUsers = users.allUsers;
+export const CardsUsers = ({ users }) => {
+  console.log("este es el console log de users",users)
   return (
     <div>
       {
-        allUsers.length
+        users?.length
           ? (
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-3 gap-x-4 2xl:gap-x-[0.5rem] rounded-md'>
-              {allUsers.map((p, i) => (
+              {users.map((p, i) => (
                 <Fragment key={i}>
                   <Link to={`/detalles/${p._id}`}>
                     <Card
