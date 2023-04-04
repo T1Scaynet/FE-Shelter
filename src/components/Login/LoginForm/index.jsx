3b3ccca07e16f06d798380adc5e1f732b18ec7fb
Estani02 <estaniolmedo@gmail.com>
2023-04-04 17:00:57 -0300

    feat: redesign form

--- src/components/Login/LoginForm/index.jsx
+++ src/components/Login/LoginForm/index.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { loginUser } from '../../../state/features/login/loginSlice';
-import ingresar from '../../../assets/ingresar.svg';
-import email from '../../../assets/email.svg';
+import ingresar from '../../../assets/ingresar.png';
 import password from '../../../assets/password.svg';
+import { Button, TextField } from '@mui/material';
+import iconGoogle from '../../../assets/iconGoogle.png';
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
@@ -33,55 +34,72 @@ export const LoginForm = () => {
         <div>
           <h3 className='text-4xl font-bold mb-4'>Iniciar Sesión</h3>
           <h2 className='text-m font-medium mb-8'>Utilice sus credenciales para acceder a su cuenta.</h2>
-          <form onSubmit={handleSubmit} className='max-w-md'>
-            <div className='mb-6'>
-              <div className='relative'>
-                <input
-                  type='email'
-                  id='email'
-                  name='email'
-                  className=' text-center w-full px-3 py-3 mb-3 leading-tight text-[#0E081E] border-none bg-[#EBE5F7] rounded-md shadow-sm focus:outline-none focus:shadow-outline'
-                  placeholder='Correo electrónico'
-                  value={login.email}
-                  onChange={handleOnChange}
-                  required
-                />
-                <img src={email} alt={email} className='absolute top-2 left-4' />
-              </div>
-
-              <div className='relative mt-6'>
-                <input
-                  type='password'
-                  id='password'
-                  name='password'
-                  className=' text-center w-full px-3 py-3 mb-3 leading-tight text-[#0E081E] border-none bg-[#EBE5F7] rounded-md shadow-sm focus:outline-none focus:shadow-outline'
-                  placeholder='Contraseña'
-                  value={login.password}
-                  onChange={handleOnChange}
-                  required
-                />
-                <img src={password} alt={password} className='absolute top-2 left-4' />
-              </div>
-
-              <Link to='/recuperar-contraseña' className=''>
-                <p className='mt-4 text-[#FF47A2] hover:text-[#7C58D3]'>¿Olvidaste tu contraseña?</p>
-              </Link>
-            </div>
+          <form onSubmit={handleSubmit} className='max-w-md flex flex-col gap-4'>
+            <TextField
+              label='Email'
+              id='email'
+              variant='outlined'
+              name='email'
+              value={login.email}
+              onChange={handleOnChange}
+              required
+              sx={{
+                '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
+                      {
+                        borderColor: '#7C58D3'
+                      },
+                '& label.Mui-focused': {
+                  color: '#7C58D3'
+                }
+              }}
+            />
+            <TextField
+              label='Contraseña'
+              type={password}
+              id='password'
+              variant='outlined'
+              name='password'
+              value={login.password}
+              onChange={handleOnChange}
+              required
+              sx={{
+                '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
+                      {
+                        borderColor: '#7C58D3'
+                      },
+                '& label.Mui-focused': {
+                  color: '#7C58D3'
+                }
+              }}
+            />
+            <Link to='/recuperar-contraseña' className=''>
+              <p className='mt-4 text-[#FF47A2] hover:text-[#7C58D3]'>¿Olvidaste tu contraseña?</p>
+            </Link>
             <div className='grid place-items-center gap-6'>
-              <button
+              <Button
                 type='submit'
-                className='w-full bg-[#7C58D3] px-4 py-2 font-bold text-white rounded hover:bg-purple-700 focus:outline-none focus:shadow-outline-blue active:bg-purple-800'
+                variant='contained'
+                style={{ backgroundColor: '#7C58D3', width: '22.188rem' }}
+                sx={{
+                  '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
+                      {
+                        borderColor: '#7C58D3'
+                      },
+                  '& label.Mui-focused': {
+                    color: '#7C58D3'
+                  }
+                }}
               >
                 Iniciar sesión
-              </button>
-              <div className='grid place-items-center'>
-                <p className=' font-semibold text-[1rem] text-[#5A5A5A]'>o inicia sesión con</p>
-                <div className='flex gap-8 mt-2'>
-                  <Link to='/'><p>Facebook</p></Link>
-                  <Link to={`${import.meta.env.VITE_REACT_APP_URL_BACKEND}/social/auth/google`}><p>Google</p></Link>
-                  <Link to='#twitter'><p>Twitter</p></Link>
+              </Button>
+              <Link to={`${import.meta.env.VITE_REACT_APP_URL_BACKEND}/social/auth/google`} className='px-[5rem] py-3 md:hover:bg-[#F1F2F2] bg-[#ffffff00] rounded-[0.5rem] md:w-[22.188rem] transition-colors duration-300'>
+                <div className='grid place-items-center'>
+                  <p className='flex flex-row justify-center items-center font-semibold text-[1rem] gap-2 text-[#5A5A5A]'>
+                    o inicia sesión con
+                    <img src={iconGoogle} alt='icon' className='h-[16px]' />
+                  </p>
                 </div>
-              </div>
+              </Link>
               <div className='flex gap-2'>
                 <p className='text-[1.1rem] font-bold text-[#0E081E]'>¿No tienes una cuenta?</p>
                 <Link to='/registro'><p className='text-[1.1rem] font-bold text-[#FF47A2]'>Registrar aquí</p></Link>
@@ -95,7 +113,7 @@ export const LoginForm = () => {
         <img
           src={ingresar}
           alt='Imagen de ingreso'
-          className='object-cover h-full w-full '
+          className='object-cover h-full w-full rounded-[0.5rem]'
         />
       </div>
     </div>
