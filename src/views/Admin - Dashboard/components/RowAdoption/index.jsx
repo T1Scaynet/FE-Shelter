f6149572675028fe87fc9fbb57029e6a41276c1f
Estani02 <estaniolmedo@gmail.com>
2023-04-04 10:23:34 -0300

    feat: Responsive table for movil

--- src/views/Admin - Dashboard/components/RowAdoption/index.jsx
+++ src/views/Admin - Dashboard/components/RowAdoption/index.jsx
@@ -35,7 +35,7 @@ export const RowAdoption = ({ info }) => {
               </div>
               <Button style={{ fontWeight: 'bold', color: '#017AFE' }} onClick={() => handleChange(data._id)}>DATOS<img src={arrowBlue} className={`w-[10px] ml-2 rotate-90 transition-all ${showInput[data._id] ? '-rotate-90' : 'rotate-90'}`} /></Button>
               <div className={`${style}`}>
-                <p className={`${data.state === 'Disponible' ? 'bg-[#BDF5D3] text-[#187d44]' : 'bg-[#ffc4cd] text-[#e00b2b]'} w-fit px-2 rounded-md`}>{data.state}</p>
+                <p className={`${data.state === 'Disponible' ? 'bg-[#BDF5D3] text-[#187d44]' : 'bg-[#ffc4cd] text-[#e00b2b]'} md:text-[1rem] text-[12px] w-fit px-2 m-auto rounded-md`}>{data.state}</p>
               </div>
               <div className={`${style} flex justify-center space-x-2`}>
                 <button className='bg-[#BDF5D3] w-7 px-1 rounded-md'>
@@ -45,30 +45,30 @@ export const RowAdoption = ({ info }) => {
                   <img src={trash} alt='Tacho de basura para eliminar' />
                 </button>
               </div>
-              <div className='w-[22.4rem] md:w-[71.825rem]'>
+              <div className='w-[22.4rem] md:w-[61rem]'>
                 <Collapse in={showInput[data._id]}>
-                  <div className='w-full h-[18rem] border-x-2 px-3 py-3 border-b-2 border-[#f0f0f0]'>
+                  <div className='w-full md:h-[18rem] border-x-2 px-3 py-3 border-b-2 border-[#f0f0f0]'>
                     <h3 className='border-b-2 border-black font-bold text-xl text-center mb-3'>Datos Pesonales</h3>
-                    <div className='flex flex-row w-full justify-between'>
+                    <div className='flex md:flex-row gap-5 flex-col w-full justify-between'>
                       <div>
-                        <b className='border-b-2 border-black'>ID</b>
-                        <p className='mt-3'><b>ID_Usuario: </b>{data.idUser}</p>
-                        <p className='mt-[0.25rem]'><b>ID_Usuario: </b>{data.idUser}</p>
+                        <b className='border-b-2 border-black md:text-[1rem] text-[0.875rem]'>ID</b>
+                        <p className='mt-3 md:text-[1rem] text-[0.75rem]'><b>ID_Usuario: </b>{data.idUser}</p>
+                        <p className='mt-[0.25rem] md:text-[1rem] text-[0.75rem]'><b>ID_Usuario: </b>{data.idUser}</p>
                       </div>
                       <div>
-                        <b className='border-b-2 border-black'>INFO</b>
-                        <p className='mt-3'><b>Nombre: </b>{data.name}</p>
-                        <p className='mt-[0.25rem]'><b>Apellido: </b>{data.lastName}</p>
-                        <p className='mt-[0.25rem]'><b>Dirección: </b>{data.address}</p>
-                        <p className='mt-[0.25rem]'><b>E-mail: </b>{data.email}</p>
-                        <p className='mt-[0.25rem]'><b>Teléfono: </b>{data.phone}</p>
-                        <p className='mt-[0.25rem]'><b>Fecha de solicitud: </b>{data.createAt}</p>
+                        <b className='border-b-2 border-black md:text-[1rem] text-[0.875rem]'>INFO</b>
+                        <p className='mt-3 md:text-[1rem] text-[0.75rem]'><b>Nombre: </b>{data.name}</p>
+                        <p className='mt-[0.25rem] md:text-[1rem] text-[0.75rem]'><b>Apellido: </b>{data.lastName}</p>
+                        <p className='mt-[0.25rem] md:text-[1rem] text-[0.75rem]'><b>Dirección: </b>{data.address}</p>
+                        <p className='mt-[0.25rem] md:text-[1rem] text-[0.75rem]'><b>E-mail: </b>{data.email}</p>
+                        <p className='mt-[0.25rem] md:text-[1rem] text-[0.75rem]'><b>Teléfono: </b>{data.phone}</p>
+                        <p className='mt-[0.25rem] md:text-[1rem] text-[0.75rem]'><b>Fecha de solicitud: </b>{data.createAt}</p>
                       </div>
                       <div>
-                        <b className='border-b-2 border-black'>DETALLES DEL HOGAR</b>
-                        <p className='mt-3'><b>Otras mascotas: </b>{data.otherPets ? 'Si' : 'No'}</p>
-                        <p className='mt-[0.25rem]'><b>Patio: </b>{data.garden ? 'Si' : 'No'}</p>
-                        <p className='mt-[0.25rem]'><b>Chicos: </b>{data.children ? 'Si' : 'No'}</p>
+                        <b className='border-b-2 border-black md:text-[1rem] text-[0.875rem]'>DETALLES DEL HOGAR</b>
+                        <p className='mt-3  md:text-[1rem] text-[0.75rem]'><b>Otras mascotas: </b>{data.otherPets ? 'Si' : 'No'}</p>
+                        <p className='mt-[0.25rem] md:text-[1rem] text-[0.75rem]'><b>Patio: </b>{data.garden ? 'Si' : 'No'}</p>
+                        <p className='mt-[0.25rem] md:text-[1rem] text-[0.75rem]'><b>Chicos: </b>{data.children ? 'Si' : 'No'}</p>
                       </div>
                     </div>
                   </div>
