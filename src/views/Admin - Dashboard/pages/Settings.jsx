0f8a878ea794dabc007aa72ab3d67603754ec4e3
Gustavo Chura <gustavochura94@gmail.com>
2023-04-04 15:39:42 -0300

    feat: update loginslice

--- src/views/Admin - Dashboard/pages/Settings.jsx
+++ src/views/Admin - Dashboard/pages/Settings.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import DefaultLayout from '../layout/DefaultLayout';
 import Breadcrumb from '../components/Breadcrumb';
 import userThree from '../images/user/user-03.png';
-import { useSelector } from 'react-redux';
+import email from '../../../assets/Settings/email.svg';
+import address from '../../../assets/Settings/home.svg';
+import dni from '../../../assets/Settings/id-card.svg';
+import telephone from '../../../assets/Settings/telephone.svg';
+import { updateUserData } from '../../../state/features/login/loginSlice';
 
 const Settings = () => {
   const user = useSelector(state => state.login.user);
+  const dispatch = useDispatch();
   const [userData, setUserData] = useState({
     name: user.name,
     lastName: user.lastName,
@@ -23,6 +30,11 @@ const Settings = () => {
     });
   };
 
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    dispatch(updateUserData(userData));
+  };
+
   return (
     <DefaultLayout>
       <div className='mx-auto max-w-270'>
@@ -37,7 +49,7 @@ const Settings = () => {
                 </h3>
               </div>
               <div className='p-7'>
-                <form action='#'>
+                <form onSubmit={handleUpdate}>
                   <div className='mb-5.5 flex flex-col gap-5.5 sm:flex-row'>
                     <div className='w-full sm:w-1/2'>
                       <label
@@ -112,29 +124,15 @@ const Settings = () => {
                     </label>
                     <div className='relative'>
                       <span className='absolute left-4.5 top-4'>
-                        <svg
+                        <img
                           className='fill-current'
                           width='20'
                           height='20'
                           viewBox='0 0 20 20'
                           fill='none'
                           xmlns='http://www.w3.org/2000/svg'
-                        >
-                          <g opacity='0.8'>
-                            <path
-                              fillRule='evenodd'
-                              clipRule='evenodd'
-                              d='M3.33301 4.16667C2.87658 4.16667 2.49967 4.54357 2.49967 5V15C2.49967 15.4564 2.87658 15.8333 3.33301 15.8333H16.6663C17.1228 15.8333 17.4997 15.4564 17.4997 15V5C17.4997 4.54357 17.1228 4.16667 16.6663 4.16667H3.33301ZM0.833008 5C0.833008 3.6231 1.9561 2.5 3.33301 2.5H16.6663C18.0432 2.5 19.1663 3.6231 19.1663 5V15C19.1663 16.3769 18.0432 17.5 16.6663 17.5H3.33301C1.9561 17.5 0.833008 16.3769 0.833008 15V5Z'
-                              fill=''
-                            />
-                            <path
-                              fillRule='evenodd'
-                              clipRule='evenodd'
-                              d='M0.983719 4.52215C1.24765 4.1451 1.76726 4.05341 2.1443 4.31734L9.99975 9.81615L17.8552 4.31734C18.2322 4.05341 18.7518 4.1451 19.0158 4.52215C19.2797 4.89919 19.188 5.4188 18.811 5.68272L10.4776 11.5161C10.1907 11.7169 9.80879 11.7169 9.52186 11.5161L1.18853 5.68272C0.811486 5.4188 0.719791 4.89919 0.983719 4.52215Z'
-                              fill=''
-                            />
-                          </g>
-                        </svg>
+                          src={email}
+                        />
                       </span>
                       <input
                         className='w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary'
@@ -156,30 +154,16 @@ const Settings = () => {
                       D.N.I
                     </label>
                     <div className='relative'>
-                      <span className='absolute left-4.5 top-4'>
-                        <svg
+                      <span className='absolute left-4 top-3'>
+                        <img
                           className='fill-current'
-                          width='20'
-                          height='20'
+                          width='30'
+                          height='30'
                           viewBox='0 0 20 20'
                           fill='none'
                           xmlns='http://www.w3.org/2000/svg'
-                        >
-                          <g opacity='0.8'>
-                            <path
-                              fillRule='evenodd'
-                              clipRule='evenodd'
-                              d='M3.33301 4.16667C2.87658 4.16667 2.49967 4.54357 2.49967 5V15C2.49967 15.4564 2.87658 15.8333 3.33301 15.8333H16.6663C17.1228 15.8333 17.4997 15.4564 17.4997 15V5C17.4997 4.54357 17.1228 4.16667 16.6663 4.16667H3.33301ZM0.833008 5C0.833008 3.6231 1.9561 2.5 3.33301 2.5H16.6663C18.0432 2.5 19.1663 3.6231 19.1663 5V15C19.1663 16.3769 18.0432 17.5 16.6663 17.5H3.33301C1.9561 17.5 0.833008 16.3769 0.833008 15V5Z'
-                              fill=''
-                            />
-                            <path
-                              fillRule='evenodd'
-                              clipRule='evenodd'
-                              d='M0.983719 4.52215C1.24765 4.1451 1.76726 4.05341 2.1443 4.31734L9.99975 9.81615L17.8552 4.31734C18.2322 4.05341 18.7518 4.1451 19.0158 4.52215C19.2797 4.89919 19.188 5.4188 18.811 5.68272L10.4776 11.5161C10.1907 11.7169 9.80879 11.7169 9.52186 11.5161L1.18853 5.68272C0.811486 5.4188 0.719791 4.89919 0.983719 4.52215Z'
-                              fill=''
-                            />
-                          </g>
-                        </svg>
+                          src={dni}
+                        />
                       </span>
                       <input
                         className='w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary'
@@ -202,29 +186,15 @@ const Settings = () => {
                     </label>
                     <div className='relative'>
                       <span className='absolute left-4.5 top-4'>
-                        <svg
+                        <img
                           className='fill-current'
                           width='20'
                           height='20'
                           viewBox='0 0 20 20'
                           fill='none'
                           xmlns='http://www.w3.org/2000/svg'
-                        >
-                          <g opacity='0.8'>
-                            <path
-                              fillRule='evenodd'
-                              clipRule='evenodd'
-                              d='M3.33301 4.16667C2.87658 4.16667 2.49967 4.54357 2.49967 5V15C2.49967 15.4564 2.87658 15.8333 3.33301 15.8333H16.6663C17.1228 15.8333 17.4997 15.4564 17.4997 15V5C17.4997 4.54357 17.1228 4.16667 16.6663 4.16667H3.33301ZM0.833008 5C0.833008 3.6231 1.9561 2.5 3.33301 2.5H16.6663C18.0432 2.5 19.1663 3.6231 19.1663 5V15C19.1663 16.3769 18.0432 17.5 16.6663 17.5H3.33301C1.9561 17.5 0.833008 16.3769 0.833008 15V5Z'
-                              fill=''
-                            />
-                            <path
-                              fillRule='evenodd'
-                              clipRule='evenodd'
-                              d='M0.983719 4.52215C1.24765 4.1451 1.76726 4.05341 2.1443 4.31734L9.99975 9.81615L17.8552 4.31734C18.2322 4.05341 18.7518 4.1451 19.0158 4.52215C19.2797 4.89919 19.188 5.4188 18.811 5.68272L10.4776 11.5161C10.1907 11.7169 9.80879 11.7169 9.52186 11.5161L1.18853 5.68272C0.811486 5.4188 0.719791 4.89919 0.983719 4.52215Z'
-                              fill=''
-                            />
-                          </g>
-                        </svg>
+                          src={address}
+                        />
                       </span>
                       <input
                         className='w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary'
@@ -247,29 +217,15 @@ const Settings = () => {
                     </label>
                     <div className='relative'>
                       <span className='absolute left-4.5 top-4'>
-                        <svg
+                        <img
                           className='fill-current'
                           width='20'
                           height='20'
                           viewBox='0 0 20 20'
                           fill='none'
                           xmlns='http://www.w3.org/2000/svg'
-                        >
-                          <g opacity='0.8'>
-                            <path
-                              fillRule='evenodd'
-                              clipRule='evenodd'
-                              d='M3.33301 4.16667C2.87658 4.16667 2.49967 4.54357 2.49967 5V15C2.49967 15.4564 2.87658 15.8333 3.33301 15.8333H16.6663C17.1228 15.8333 17.4997 15.4564 17.4997 15V5C17.4997 4.54357 17.1228 4.16667 16.6663 4.16667H3.33301ZM0.833008 5C0.833008 3.6231 1.9561 2.5 3.33301 2.5H16.6663C18.0432 2.5 19.1663 3.6231 19.1663 5V15C19.1663 16.3769 18.0432 17.5 16.6663 17.5H3.33301C1.9561 17.5 0.833008 16.3769 0.833008 15V5Z'
-                              fill=''
-                            />
-                            <path
-                              fillRule='evenodd'
-                              clipRule='evenodd'
-                              d='M0.983719 4.52215C1.24765 4.1451 1.76726 4.05341 2.1443 4.31734L9.99975 9.81615L17.8552 4.31734C18.2322 4.05341 18.7518 4.1451 19.0158 4.52215C19.2797 4.89919 19.188 5.4188 18.811 5.68272L10.4776 11.5161C10.1907 11.7169 9.80879 11.7169 9.52186 11.5161L1.18853 5.68272C0.811486 5.4188 0.719791 4.89919 0.983719 4.52215Z'
-                              fill=''
-                            />
-                          </g>
-                        </svg>
+                          src={telephone}
+                        />
                       </span>
                       <input
                         className='w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary'
@@ -283,7 +239,7 @@ const Settings = () => {
                     </div>
                   </div>
 
-                  <div className='mb-5.5'>
+                  {/* <div className='mb-5.5'>
                     <label
                       className='mb-3 block text-sm font-medium text-black dark:text-white'
                       htmlFor='Username'
@@ -298,9 +254,9 @@ const Settings = () => {
                       placeholder='devidjhon24'
                       defaultValue='devidjhon24'
                     />
-                  </div>
+                  </div> */}
 
-                  <div className='mb-5.5'>
+                  {/* <div className='mb-5.5'>
                     <label
                       className='mb-3 block text-sm font-medium text-black dark:text-white'
                       htmlFor='Username'
@@ -348,7 +304,7 @@ const Settings = () => {
                         defaultValue='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere fermentum urna, eu condimentum mauris tempus ut. Donec fermentum blandit aliquet.'
                       />
                     </div>
-                  </div>
+                  </div> */}
 
                   <div className='flex justify-end gap-4.5'>
                     <button
