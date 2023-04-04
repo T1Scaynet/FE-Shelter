29b3ed70931cd88471b862d94893a16fc21db670
Estani02 <estaniolmedo@gmail.com>
2023-04-04 10:22:27 -0300

    chore: mobile text

--- src/views/Admin - Dashboard/components/RowTitles/index.jsx
+++ src/views/Admin - Dashboard/components/RowTitles/index.jsx
@@ -3,7 +3,7 @@ export const RowTitles = ({ titles }) => {
     <div className={`grid grid-cols-${titles.length} bg-black`}>
       {
         titles && titles.map((t, i) =>
-          <p className='text-[white] py-3 px-3 border-r-[1px] border-solid border-[#FFFFFF]' key={i}>{t}</p>
+          <p className='text-[white] py-3 px-3 border-r-[1px] border-solid border-[#FFFFFF] text-[0.8rem] md:text-[1rem]' key={i}>{t}</p>
         )
       }
     </div>
