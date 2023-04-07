4037c116424ba77b229a24d1bd0b883326bb46c0
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-04-07 19:02:51 -0300

    chore: colors of updated elements

--- src/components/Login/Profile/profile-comment.jsx
+++ src/components/Login/Profile/profile-comment.jsx
@@ -32,7 +32,7 @@ export const profileComment = () => {
           className='grid grid-cols-5 gap-8'
         >
           <div className='col-span-5 xl:col-span-3'>
-            <div className='rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark'>
+            <div className='rounded-sm border border-stroke bg-red shadow-default dark:border-strokedark dark:bg-boxdark'>
               <div className='border-b border-stroke py-4 px-7 dark:border-strokedark'>
                 <h3 className='font-medium text-black dark:text-white'>
                   Hacer una reseña a la fundación
