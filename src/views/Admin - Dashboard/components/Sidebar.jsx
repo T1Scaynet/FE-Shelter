33c52819789f93e956749d399e075337bf91c785
Gustavo Chura <gustavochura94@gmail.com>
2023-04-13 11:37:10 -0300

    feat: arrow down added and change logo by panel de control

--- src/views/Admin - Dashboard/components/Sidebar.jsx
+++ src/views/Admin - Dashboard/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import SidebarLinkGroup from './SidebarLinkGroup';
-import Logo from '../images/logo/logo.svg';
+// import Logo from '../images/logo/logo.svg';
 import pets from '../assets/pets.svg';
 import users from '../assets/users.svg';
 import donations from '../assets/donations.svg';
@@ -10,6 +10,7 @@ import calendar from '../assets/calendar.svg';
 // import user from '../assets/user.svg';
 import adoption from '../assets/adoption.svg';
 import settings from '../assets/settings.svg';
+import arrow from '../assets/arrow-down.svg';
 
 const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
   const location = useLocation();
@@ -68,8 +69,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className='flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5'>
-        <NavLink to='/'>
-          <img src={Logo} alt='Logo' />
+        <NavLink to='/' className='w-full h-full'>
+          {/* <img src={Logo} alt='Logo' /> */}
+          <h1 className=' text-[#FFFF] font-bold  text-3xl text-center'>Panel de Control</h1>
         </NavLink>
 
         <button
@@ -140,23 +142,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           src={dashboard}
                         />
                         Dashboard
-                        <svg
+                        <img
                           className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
                             open && 'rotate-180'
                           }`}
-                          width='20'
-                          height='20'
+                          width='14'
+                          height='14'
                           viewBox='0 0 20 20'
                           fill='none'
                           xmlns='http://www.w3.org/2000/svg'
-                        >
-                          <path
-                            fillRule='evenodd'
-                            clipRule='evenodd'
-                            d='M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z'
-                            fill=''
-                          />
-                        </svg>
+                          src={arrow}
+                        />
                       </NavLink>
 
                       {/* <!-- Dropdown Menu Start --> */}
@@ -263,23 +259,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           src={pets}
                         />
                         Mascotas
-                        <svg
+                        <img
                           className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
                             open && 'rotate-180'
                           }`}
-                          width='20'
-                          height='20'
+                          width='14'
+                          height='14'
                           viewBox='0 0 20 20'
                           fill='none'
                           xmlns='http://www.w3.org/2000/svg'
-                        >
-                          <path
-                            fillRule='evenodd'
-                            clipRule='evenodd'
-                            d='M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z'
-                            fill=''
-                          />
-                        </svg>
+                          src={arrow}
+                        />
                       </NavLink>
                       {/* <!-- Dropdown Menu Start --> */}
                       <div
@@ -350,23 +340,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           src={users}
                         />
                         Usuarios
-                        <svg
+                        <img
                           className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
                             open && 'rotate-180'
                           }`}
-                          width='20'
-                          height='20'
+                          width='14'
+                          height='14'
                           viewBox='0 0 20 20'
                           fill='none'
                           xmlns='http://www.w3.org/2000/svg'
-                        >
-                          <path
-                            fillRule='evenodd'
-                            clipRule='evenodd'
-                            d='M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z'
-                            fill=''
-                          />
-                        </svg>
+                          src={arrow}
+                        />
                       </NavLink>
                       {/* <!-- Dropdown Menu Start --> */}
                       <div
@@ -437,23 +421,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           src={donations}
                         />
                         Donaciones
-                        <svg
+                        <img
                           className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
                             open && 'rotate-180'
                           }`}
-                          width='20'
-                          height='20'
+                          width='14'
+                          height='14'
                           viewBox='0 0 20 20'
                           fill='none'
                           xmlns='http://www.w3.org/2000/svg'
-                        >
-                          <path
-                            fillRule='evenodd'
-                            clipRule='evenodd'
-                            d='M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z'
-                            fill=''
-                          />
-                        </svg>
+                          src={arrow}
+                        />
                       </NavLink>
                       {/* <!-- Dropdown Menu Start --> */}
                       <div
@@ -514,23 +492,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           src={adoption}
                         />
                         Adopciones
-                        <svg
+                        <img
                           className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
                             open && 'rotate-180'
                           }`}
-                          width='20'
-                          height='20'
+                          width='14'
+                          height='14'
                           viewBox='0 0 20 20'
                           fill='none'
                           xmlns='http://www.w3.org/2000/svg'
-                        >
-                          <path
-                            fillRule='evenodd'
-                            clipRule='evenodd'
-                            d='M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z'
-                            fill=''
-                          />
-                        </svg>
+                          src={arrow}
+                        />
                       </NavLink>
                       {/* <!-- Dropdown Menu Start --> */}
                       <div
