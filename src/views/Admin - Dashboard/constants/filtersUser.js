2e622a172fca06d1f17ee12b338af371d773c735
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-04-13 23:01:54 -0300

    feat: option filters user created

---
+++ src/views/Admin - Dashboard/constants/filtersUser.js
@@ -0,0 +1,28 @@
+export const filtersUser = [
+  {
+    title: 'Estado',
+    optionOne: 'Activo',
+    valueOne: 'true',
+    optionTwo: 'Inactivo',
+    valueTwo: 'false',
+    param: 'active'
+  },
+  {
+    title: 'Ordenar',
+    optionOne: 'A - Z',
+    optionAZ: 'alphabetical',
+    optionTwo: 'Z - A',
+    optionZA: 'alphabetical_desc',
+    param: 'sort'
+  },
+  {
+    title: 'Rol',
+    optionOne: 'Admin',
+    valueOne: '641b1cb89c7b609fb708ed8a',
+    optionTwo: 'Moderador',
+    valueTwo: '641b1cb89c7b609fb708ed89',
+    optionThree: 'Usuario',
+    valueThree: '641b1cb89c7b609fb708ed88',
+    param: 'roles'
+  }
+];
