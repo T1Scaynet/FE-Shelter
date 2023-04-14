f615b2fd5a717de2dc6075df95c374e437f2ac9d
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-04-13 23:02:56 -0300

    feat: states pagintation and filters added in userSlice

--- src/state/features/users/userSlice.js
+++ src/state/features/users/userSlice.js
@@ -4,100 +4,59 @@ import axios from 'axios';
 export const userSlice = createSlice({
   name: 'users',
   initialState: {
-    // name, email, roles, sortBy, search, page, limit
     list: [],
-    logged: false,
-    allUsers: []
+    pagination: {
+      totalPage: 1,
+      currentPage: 1
+    },
+    filters: {
+      active: '',
+      roles: '',
+      sort: '',
+      totalPages: 1,
+      currentPage: 1,
+      search: ''
+    }
   },
   reducers: {
-    setUser: (state, action) => {
-      state.user = action.payload; /// Aca vendria los datos que me manden de login o register (info del user)
-    },
-    setUserLogout: (state, action) => {
-      state.user = null;
-    },
     setAllUsers: (state, action) => {
-      state.allUsers = action.payload;
       state.list = action.payload;
+    },
+    setPagination: (state, action) => {
+      state.pagination = action.payload;
+    },
+    setFilters: (state, action) => {
+      state.filters = action.payload;
+    },
+    setUpdateUser: (state, action) => {
+      state.list = { ...state.list, ...action.payload };
     }
   }
 });
-export const { setUser, setUserLogout, setAllUsers } = userSlice.actions;
+export const { setAllUsers, setPagination, setFilters, setUpdateUser } = userSlice.actions;
 
 export default userSlice.reducer;
 
-// name, email, roles, sortBy, search, page, limit
-export const getAllUsers = () => {
+export const getAllUsers = ({
+  active = '',
+  roles = '',
+  sort = '',
+  currentPage = 1,
+  search = ''
+}) => {
   return async function (dispatch) {
-    axios.get('/user')
-      .then(r => r.data)
+    axios.get(`/user?search=${search}&page=${currentPage}&roles=${roles}&active=${active}&sort=${sort}`)
+      .then(r => (r.data))
       .then(response => {
-        dispatch(setAllUsers(response.users));
-      });
+        dispatch(setFilters({ active, roles, sort, search, currentPage }));
+        dispatch(setAllUsers(response.forms));
+        dispatch(
+          setPagination({
+            totalPages: response.totalPages,
+            currentPage: response.currentPage
+          })
+        );
+      })
+      .catch(() => dispatch(setFilters({})));
   };
 };
-
-// axios.get(`/user?search=${search}&page=${currentPage}&name=${name}&email=${email}&roles=${roles}&sort=${sortBy}`)
-//   .then((r) => r.data)
-//   .then((response) => {
-//     console.log('este console log que trae', response);
-//     dispatch(setAllUsers(response.users));
-//     dispatch(
-//       setPagination({
-//         totalPages: response.totalPages,
-//         currentPage: response.currentPage
-//       })
-//     );
-//   })
-//   .catch((e) => console.log(e));
-
-// export const getAllUsers = ({
-//   name = '',
-//   email = '',
-//   roles = '',
-//   sortBy = '',
-//   currentPage = '',
-//   search = ''
-// }) => {
-//   return async function (_dispatch, getState) {
-//     const currentState = getState().login;
-//     try {
-//       console.log(currentState.token);
-//       const instance = axios.create();
-//       instance.defaults.headers.common['x-access-token'] = currentState.token;
-//       const sendaxios = await instance.get(
-//         `/user?search=${search}&page=${currentPage}&name=${name}&email=${email}&roles=${roles}&sort=${sortBy}`
-//       );
-//       console.log(sendaxios)
-//       return sendaxios;
-//     } catch (error) {
-//       console.warn("Error al enviar datos en función 'PostPet'");
-//       return error;
-//     }
-//   };
-// };
-
-// import axios from 'axios';
-
-// export const getAllUsers = ({ name = '', email = '', roles = '', sortBy = '', currentPage = '', search = '' }) => {
-//   return async (dispatch, getState) => {
-//     const { token } = getState().login;
-
-//     try {
-//       const instance = axios.create({
-//         headers: {
-//           'x-access-token': token
-//         }
-//       });
-
-//       const response = await instance.get(`/user?search=${search}&page=${currentPage}&name=${name}&email=${email}&roles=${roles}&sort=${sortBy}`);
-
-//       console.log(response.data);
-//       return response.data;
-//     } catch (error) {
-//       console.warn("Error al enviar datos en función 'getAllUsers'");
-//       console.error(error);
-//       throw error;
-//     }
-//   };
-// };
