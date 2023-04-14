242cc2530dab898965ff18bf3731358e0817b341
Gustavo Chura <gustavochura94@gmail.com>
2023-04-14 12:02:13 -0300

    feat: refactor the code

--- src/views/Admin - Dashboard/pages/Donaciones/Donaciones.jsx
+++ src/views/Admin - Dashboard/pages/Donaciones/Donaciones.jsx
@@ -1,76 +1,100 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getDonations } from '../../../../state/features/donations/donationSlice';
+import { getDonations, setFilters1 } from '../../../../state/features/donations/donationSlice';
 import Breadcrumb from '../../components/Breadcrumb';
 import DefaultLayout from '../../layout/DefaultLayout';
 import { RowTitles } from '../../components/RowTitles';
 import { titlesDonations } from '../../constants/titlesDonations';
 import { RowDonations } from '../../components/RowDonations';
 import { FiltersDonations } from '../../components/FiltersDonations';
+import { Pagination } from '../../../../components/Pagination';
 
 export const DonacionesPagos = () => {
   const dispatch = useDispatch();
-  const donationsList = useSelector((state) => state.donations.donations);
-  useEffect(() => {
-    dispatch(getDonations());
-  }, [dispatch]);
-  const [selectedStatus, setSelectedStatus] = useState('all');
-  const handleStatusChange = (event) => {
-    setSelectedStatus(event.target.value);
+  const donationsList = useSelector((state) => state.donations.list);
+  const pagination = useSelector(state => state.donations.pagination);
+
+  const handlePageChange = (pageNumber) => {
+    dispatch(getDonations({
+      // ...donationsList,
+      currentPage: pageNumber
+    }));
   };
+
+  // const [selectedStatus, setSelectedStatus] = useState('all');
+  // const handleStatusChange = (event) => {
+  //   setSelectedStatus(event.target.value);
+  // };
 
   const [selectedDate, setSelectedDate] = useState('');
   const handleDateChange = (date) => {
-    setSelectedDate(date);
+    // console.log(new Date(date).toLocaleString('es-AR'));
+    dispatch(setFilters1(new Date(date).toLocaleString('es-AR')));
   };
 
-  const [searchText, setSearchText] = useState('');
+  // const [searchText, setSearchText] = useState('');
 
-  const filterDonations = () => {
-    return donationsList.filter((donation) => {
-      const searchRegex = new RegExp(searchText, 'i');
-      return searchRegex.test(donation.idUser) || searchRegex.test(donation.title);
-    }).filter((donation) => {
-      if (selectedStatus === 'all') {
-        return true;
-      } else {
-        return donation.status === selectedStatus;
-      }
-    }).filter((donation) => {
-      if (selectedDate === '') {
-        return true;
-      } else {
-        return new Date(donation.createAt).toLocaleDateString('es-AR') === selectedDate.toLocaleDateString('es-AR');
-      }
-    });
-  };
+  // const filterDonations = () => {
+  //   return donationsList?.filter((donation) => {
+  //     const searchRegex = new RegExp(searchText, 'i');
+  //     return searchRegex.test(donation.idUser) || searchRegex.test(donation.title);
+  //   }).filter((donation) => {
+  //     if (selectedStatus === 'all') {
+  //       return true;
+  //     } else {
+  //       return donation.status === selectedStatus;
+  //     }
+  //   }).filter((donation) => {
+  //     if (selectedDate === '') {
+  //       return true;
+  //     } else {
+  //       return new Date(donation.createAt).toLocaleDateString('es-AR') === selectedDate.toLocaleDateString('es-AR');
+  //     }
+  //   });
+  // };
 
   // paginado//
-  const [currentPage, setCurrentPage] = useState(1); // Inicializamos currentPage con useState de React
+  // const [currentPage, setCurrentPage] = useState(1); // Inicializamos currentPage con useState de React
   // eslint-disable-next-line no-unused-vars
-  const [itemsPerPage, setItemsPerPage] = useState(5); // Inicializamos itemsPerPage con useState de React
-  const totalPages = Math.ceil(filterDonations().length / itemsPerPage);
+  // const [itemsPerPage, setItemsPerPage] = useState(5); // Inicializamos itemsPerPage con useState de React
+  // const totalPages = Math.ceil(filterDonations().length / itemsPerPage);
 
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
+  // const handlePageChange = (pageNumber) => {
+  //   setCurrentPage(pageNumber);
+  // };
 
-  const getDonationsForPage = () => {
-    const filteredDonations = filterDonations();
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    return filteredDonations.slice(startIndex, endIndex);
-  };
+  // const getDonationsForPage = () => {
+  //   const filteredDonations = filterDonations();
+  //   const startIndex = (currentPage - 1) * itemsPerPage;
+  //   const endIndex = startIndex + itemsPerPage;
+  //   return filteredDonations.slice(startIndex, endIndex);
+  // };
+
+  useEffect(() => {
+    const currentPage = pagination.currentPage;
+    dispatch(getDonations({ currentPage }));
+  }, []);
+
   return (
     <DefaultLayout>
       <Breadcrumb pageName='Donaciones' />
       <FiltersDonations
-        searchText={searchText} setSearchText={setSearchText} selectedStatus={selectedStatus}
-        handleStatusChange={handleStatusChange} selectedDate={selectedDate} handleDateChange={handleDateChange}
+        // searchText={searchText}
+        // setSearchText={setSearchText}
+        // selectedStatus={selectedStatus}
+        // handleStatusChange={handleStatusChange}
+        selectedDate={selectedDate}
+        handleDateChange={handleDateChange}
       />
       <RowTitles titles={titlesDonations} />
-      <RowDonations info={getDonationsForPage()} />
-      <div className='flex justify-end'>
+      <RowDonations info={donationsList} />
+      <Pagination
+        isDashboard
+        currentPage={pagination.currentPage}
+        totalPages={pagination.totalPages}
+        onPageChange={handlePageChange}
+      />
+      {/* <div className='flex justify-end'>
         <div className='pagination rounded-full'>
           <button
             onClick={() => handlePageChange(currentPage - 1)}
@@ -98,7 +122,7 @@ export const DonacionesPagos = () => {
             {'>'}
           </button>
         </div>
-      </div>
+      </div> */}
 
     </DefaultLayout>
   );
